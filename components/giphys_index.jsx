49b385babe026d8cd26ef0ac7f4349c489d4cf10
beebeean09 <vivianlee@Vivians-MacBook-Air.local>
2017-03-08 00:10:38 -0800

    complete giphysIndex

--- components/giphys_index.jsx
+++ components/giphys_index.jsx
@@ -1,5 +1,26 @@
 import React from 'react';
 
+class GiphysIndex extends React.Component {
 
+  render() {
+    const { giphys } = this.props;
+
+    const giphysIndexItems = giphys => {
+      return giphys.map((giphy, idx) => (
+        <li key={idx} className="giphy-li">
+          <img src={giphy.images.fixed_height.url} />
+        </li>
+        )
+      );
+    };
+
+    return(
+      <div>
+        {giphysIndexItems(giphys)}
+      </div>
+
+    );
+  }
+}
 
 export default GiphysIndex;
